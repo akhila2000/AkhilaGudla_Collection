28576101ac68b4cea3e8813edce64457a711ac57
akhila2000 <gakhilagoud1234@gmail.com>
2020-03-03 22:23:04 +0530

    first commit

---
+++ MyList/src/test/java/com/vnrvjiet/Akhila_Collections/AppTest.java
@@ -0,0 +1,20 @@
+package com.vnrvjiet.Akhila_Collections;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
